26a3b04dff8719bd5bda10d120746184e01f3124
Tiago Ramos Aguiar <126036647+tiagoramosaguiar@users.noreply.github.com>
2023-03-14 00:55:12 -0300

    Update Atividade04.java

--- Atividades/Atividade04.java
+++ Atividades/Atividade04.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 
 public class Atividade04 {
     public static void main(String[] args) {
-        Integer a, b;
         Scanner scan = new Scanner(System.in);
+        Integer a, b;
+        
         System.out.print("Insira valor de A (número inteiro): ");
         a = scan.nextInt();
         System.out.print("Insira valor de B (número inteiro): ");
         b = scan.nextInt();
+        
         a = a + b;
         b = a - b;
         a = a - b;
+        
         System.out.println("↓");
         System.out.println("Valor de A após troca: " + a);
         System.out.println("Valor de B após troca: " + b);
