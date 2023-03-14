6f4aa3c519e7e63c4e5bd5804ef7267e846145aa
Tiago Ramos Aguiar <126036647+tiagoramosaguiar@users.noreply.github.com>
2023-03-14 00:54:39 -0300

    Update Atividade03.java

--- Atividades/Atividade03.java
+++ Atividades/Atividade03.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 
 public class Atividade03 {
     public static void main(String[] args) {
-        float celsius, fahrenheit;
         Scanner scan = new Scanner(System.in);
+        float celsius, fahrenheit;
+        
         System.out.print("Insira a temperatura em Celsius: ");
         celsius = scan.nextFloat();
         fahrenheit = (9 * celsius + 160) / 5;
+        
         System.out.println("↓");
         System.out.printf("Temperatura em Fahrenheit: %.1f °F", fahrenheit);
     }
