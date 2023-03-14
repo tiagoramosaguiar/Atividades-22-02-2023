7141531435a825ca91d1487dab19fac6b5055724
Tiago Ramos Aguiar <126036647+tiagoramosaguiar@users.noreply.github.com>
2023-03-14 00:55:51 -0300

    Update Atividade05.java

--- Atividades/Atividade05.java
+++ Atividades/Atividade05.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 
 public class Atividade05 {
     public static void main(String[] args) {
-        int numero1, numero2, restodiv, quociente;
         Scanner scan = new Scanner(System.in);
+        int numero1, numero2, restodiv, quociente;
+        
         System.out.print("Insira um número (inteiro): ");
         numero1 = scan.nextInt();
         System.out.print("Insira um número (inteiro): ");
         numero2 = scan.nextInt();
+        
         restodiv = numero1 % numero2;
         quociente = numero1 / numero2;
+        
         System.out.println("↓");
         System.out.println("Quociente: " + quociente);
         System.out.println("Resto da divisão: " + restodiv);
