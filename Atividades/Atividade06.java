ffc2a532ea8193370359af8c8c31b2f00fce5cb9
Tiago Ramos Aguiar <126036647+tiagoramosaguiar@users.noreply.github.com>
2023-03-14 00:56:15 -0300

    Update Atividade06.java

--- Atividades/Atividade06.java
+++ Atividades/Atividade06.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 
 public class Atividade06 {
     public static void main(String[] args) {
-        int a, b, soma;
         Scanner scan = new Scanner(System.in);
+        int a, b, soma;
+        
         System.out.print("Insira valor de A (número inteiro): ");
         a = scan.nextInt();
         System.out.print("Insira valor de B (número inteiro): ");
         b = scan.nextInt();
+        
         soma = (a * a) + (b * b);
+        
         System.out.println("↓");
         System.out.println("Resultado do quadrado da soma: " + soma);
     }
