10abfdb30f1ed6edd807e3b28f9e12b47474d172
Tiago Ramos Aguiar <126036647+tiagoramosaguiar@users.noreply.github.com>
2023-03-14 00:54:00 -0300

    Update Atividade02.java

--- Atividades/Atividade02.java
+++ Atividades/Atividade02.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 
 public class Atividade02 {
     public static void main(String[] args) {
-        int numero1, numero2, soma;
 	Scanner scan = new Scanner(System.in);
+        int numero1, numero2, soma;
+	    
         System.out.print("Insira um número (inteiro): ");
         numero1 = scan.nextInt();
         System.out.print("Insira um número (inteiro): ");
         numero2 = scan.nextInt();
+	    
         soma = numero1 + numero2;
+	
         System.out.println("↓");
         System.out.println("Soma dos números: " + soma);
     }
