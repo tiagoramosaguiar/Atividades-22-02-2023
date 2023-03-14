6fb5daee00199748ed538175cfa29e420fc2c7e6
Tiago Ramos Aguiar <126036647+tiagoramosaguiar@users.noreply.github.com>
2023-03-14 00:55:35 -0300

    Update Atividade04_2.java

--- Atividades/Atividade04_2.java
+++ Atividades/Atividade04_2.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 
 public class Atividade04_2 {
     public static void main(String[] args) {
-        String a, b, auxiliar;
         Scanner scan = new Scanner(System.in);
+        String a, b, auxiliar;
+        
         System.out.print("Insira valor de A: ");
         a = scan.nextLine();
         System.out.print("Insira valor de B: ");
         b = scan.nextLine();
+        
         auxiliar = a;
         a = b;
         b = auxiliar;
+        
         System.out.println("↓");
         System.out.println("Valor de A após troca: " + a);
         System.out.println("Valor de B após troca: " + b);
