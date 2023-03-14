a4f1b6a7841de9b72578378e8fe7b77af164e8aa
Tiago Ramos Aguiar <126036647+tiagoramosaguiar@users.noreply.github.com>
2023-03-14 00:53:12 -0300

    Update Atividade01.java

--- Atividades/Atividade01.java
+++ Atividades/Atividade01.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 
 public class Atividade01 {
     public static void main (String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
         float nota1, nota2, nota3, media;
         String nomeAluno;
-        Scanner scan = new Scanner(System.in);
+        
         System.out.print("Insira nome do aluno: ");
         nomeAluno = scan.nextLine();
         System.out.print("Insira primeira nota: ");
@@ -14,13 +16,17 @@ public class Atividade01 {
         nota2 = scan.nextFloat();
         System.out.print("Insira terceira nota: ");
         nota3 = scan.nextFloat();
+        
         media = (nota1 + nota2 + nota3) / 3;
+        
         System.out.println("↓");
         System.out.printf("Média do aluno " + nomeAluno + ": %.2f -> ", media);
+        
         if (media >= 6) {
             System.out.println("Aprovado!");
         } else {
             System.out.println("Reprovado!");
         }
+        
     }
 }
